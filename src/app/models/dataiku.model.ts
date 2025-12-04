fe26250cbc651c992d2f589c29921e4c6f542d93
François Brouchoud <francois.brouchoud@hevs.ch>
2025-12-04 22:42:23 +0100

    feat : new model for dataiku payload and specifc result

---
+++ src/app/models/dataiku.model.ts
@@ -0,0 +1,49 @@
+export interface DataikuPredictionResponse {
+  result: {
+    prediction: string | number;
+    probaPercentile: number;
+    probas: Record<string, number>;
+    ignored: boolean;
+  };
+  timing: Record<string, number>;
+  apiContext: {
+    serviceId: string;
+    endpointId: string;
+    serviceGeneration: string;
+  };
+}
+
+/**
+ * Payload envoyé à Dataiku pour OnlyURLSpecs.
+ */
+export interface DataikuOnlyUrlSpecsFeatures {
+  NoOfOtherSpecialCharsInURL: string;
+  LetterRatioInURL: string;
+  SpecialCharRatioInURL: string;
+  DegitRatioInURL: string;
+  NoOfLettersInURL: string;
+  NoOfDegitsInURL: string;
+  TLD: string;
+  URLLength: number;
+  NoOfSubDomain: string;
+  CharContinuationRate: string;
+  DomainLength: string;
+  TLDLength: string;
+  Domain: string;
+  NoOfQMarkInURL: string;
+  NoOfEqualsInURL: string;
+  NoOfAmpersandInURL: string;
+  IsDomainIP: string;
+}
+
+export interface DataikuPredictionRequest {
+  features: DataikuOnlyUrlSpecsFeatures;
+}
+
+export interface DataikuResult {
+  prediction: string;
+  probaPercentile: number;
+  probas: Record<string, number>;
+  ignored: boolean;
+}
+
