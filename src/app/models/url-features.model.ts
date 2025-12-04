57c35df2fbffee57692925364ca1186703771f5f
François Brouchoud <francois.brouchoud@hevs.ch>
2025-12-04 21:11:35 +0100

    refactor model

--- src/app/models/url-features.model.ts
+++ src/app/models/url-features.model.ts
@@ -1,24 +1,19 @@
 export interface UrlFeatures {
   [x: string]: any;
-  // URL brute utilisée pour les stats
+
   URL: string;
   URLLength: number;
 
-  // Host complet, y compris "www"
   Domain: string;
   DomainLength: number;
 
-  // IP ou nom de domaine
   IsDomainIP: number;
 
-  // Dernier label du host (ex: com, fr, io, app, 123 pour une IP)
   TLD: string;
   TLDLength: number;
 
-  // Nombre de sous-domaines = max(0, nb_labels - 2)
   NoOfSubDomain: number;
 
-  // Caractères
   NoOfLettersInURL: number;
   LetterRatioInURL: number;
 
@@ -28,11 +23,9 @@ export interface UrlFeatures {
   NoOfEqualsInURL: number;
   NoOfQMarkInURL: number;
   NoOfAmpersandInURL: number;
-  NoOfOtherSpecialCharsInURL: number;
 
-  // (otherSpecials + ? + & + =) / URLLength
+  NoOfOtherSpecialCharsInURL: number;
   SpecialCharRatioInURL: number;
 
-  // Continuité des classes de caractères (lettre / chiffre / autre)
   CharContinuationRate: number;
 }
